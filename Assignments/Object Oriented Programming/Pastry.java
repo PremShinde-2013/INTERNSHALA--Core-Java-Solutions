a2d2f8cb580988187b6ee16868d3be46b7ce566e
Premshinde123 <118251394+Premshinde123@users.noreply.github.com>
2022-11-27 14:06:35 +0530

    Instructor's Solution

---
+++ Assignments/Object Oriented Programming/Pastry.java
@@ -0,0 +1,8 @@
+/*** Pastry Class ***/
+public class Pastry extends Cake {
+
+    @Override
+    public void display() {
+        System.out.println("  " + name + " : " + '\u20B9' + " " + price + " per piece");
+    }
+}
